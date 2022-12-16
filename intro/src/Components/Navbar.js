e58481be596fdf043adfe26b0e4700e74132296b
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2022-12-16 12:49:36 +0100

    adding sass

---
+++ intro/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <a href="/home"> Home</a>
+            <a href="/about"> About</a>
+            <a href="/profile"> Profile</a>
+            <a href="/contact"> Contact</a>
+        </div>
+    );
+}
+
+export default Navbar;
